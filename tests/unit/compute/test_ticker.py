a3b22b50e2934723ad66257031b506ec0965ffd3
mdambski <19229917+mdambski@users.noreply.github.com>
2025-01-15 23:53:34 +0100

    refactor and add tests

--- tests/unit/compute/test_ticker.py
+++ tests/unit/compute/test_ticker.py
@@ -28,6 +28,13 @@ def treasury_yield_data():
 
 
 @pytest.fixture
+def yield_data():
+    return pd.DataFrame(
+        {"Yield": [0.2, 0.3, 0.4]}, index=pd.date_range("2024-01-01", periods=3)
+    )
+
+
+@pytest.fixture
 def ticker_name():
     return "HYG"
 
@@ -74,45 +81,67 @@ def compute_dividend_mock(dividend_data):
 
 
 @pytest.fixture
-def compute_yield_mock(compute_dividend_mock):
+def compute_yield_mock(compute_dividend_mock, yield_data):
     with patch("liquidity.compute.ticker.compute_dividend_yield") as m:
+        m.return_value = yield_data
         yield m
 
 
 class TestTicker:
-    def test_prices_property(self, ticker, mock_provider):
-        _ = ticker._get(ticker._get_key("prices"), ticker._fetch_prices)
+    def test_prices_property(self, ticker, mock_provider, price_data):
+        df = ticker.prices
+
         mock_provider.get_prices.assert_called_once_with(ticker.name)
+        pd.testing.assert_frame_equal(df, price_data)
 
     def test_dividends_property(
         self, ticker, dividend_data, mock_metadata, compute_dividend_mock
     ):
-        _ = ticker._get(ticker._get_key("dividends"), ticker._fetch_dividends)
+        df = ticker.dividends
+
         compute_dividend_mock.assert_called_once_with(
             dividend_data, mock_metadata.distribution_frequency
         )
+        pd.testing.assert_frame_equal(df, dividend_data)
 
     def test_yields_property_for_treasury(
-        self, ticker, mock_provider, mock_metadata, price_data, dividend_data
+        self,
+        ticker,
+        mock_provider,
+        mock_metadata,
+        price_data,
+        dividend_data,
+        treasury_yield_data,
     ):
         mock_metadata.is_treasury_yield = True
         mock_metadata.maturity = "10year"
-        _ = ticker._get(ticker._get_key("yields"), ticker._fetch_yields)
+
+        df = ticker.yields
+
         mock_provider.get_treasury_yield.assert_called_once_with(mock_metadata.maturity)
+        pd.testing.assert_frame_equal(df, treasury_yield_data)
 
     def test_yields_property_for_non_treasury(
-        self, ticker, compute_yield_mock, mock_metadata, price_data, dividend_data
+        self,
+        ticker,
+        compute_yield_mock,
+        mock_metadata,
+        price_data,
+        dividend_data,
+        yield_data,
     ):
         mock_metadata.is_treasury_yield = False
-        _ = ticker._get(ticker._get_key("yields"), ticker._fetch_yields)
+
+        df = ticker.yields
+
         compute_yield_mock.assert_called_once_with(price_data, dividend_data)
+        pd.testing.assert_frame_equal(df, yield_data)
 
     def test_get_key_method(self, ticker):
         assert ticker._get_key("prices") == "HYG-prices"
 
     def test_get_method_cache_miss(self, ticker, mock_provider):
-        cache_key = ticker._get_key("prices")
-        ticker._get(cache_key, ticker._fetch_prices)
+        _ = ticker.prices
         mock_provider.get_prices.assert_called_once()
 
     def test_get_method_cache_hit(self, ticker, mock_provider):
@@ -120,5 +149,5 @@ class TestTicker:
         ticker.cache[cache_key] = pd.DataFrame(
             {"Price": [100, 101, 102]}, index=pd.date_range("2025-01-01", periods=3)
         )
-        ticker._get(cache_key, ticker._fetch_prices)
+        _ = ticker.prices
         mock_provider.get_prices.assert_not_called()
